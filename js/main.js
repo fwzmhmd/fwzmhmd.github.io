9e656fc64fb68a776c50dfec31f063218c49cbe7
Fawaz Mohamed <fwz.mhmd@gmail.com>
2017-09-28 12:48:46 +0100

    Rewrite JavaScript for website redesign

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-	// Show/hide elements depending on window size
-	if ($(window).width() <= 1024) {
-		$("nav").hide();
-	} else if ($(window).width() > 1024) {
-		$("nav").show();
-		$(".nav-btn").hide();
-	};
-
-	// Toggle for the dropdown
+	// Toggle for dropdown
 	$(".dropbtn").click(function () {
 		$("nav").slideToggle();
 		$(this).toggleClass("transform");
+		$("header").toggleClass("headerbg");		
 	});
+
+    // Get window scroll position
+    var scrollPosition = $(window).scrollTop();
+
+    // Function to add background if scrolled
+    function checkScroll(){
+        var startY = $("header").height() + 10;
+        if($(window).scrollTop() > startY) {
+            $("header").addClass("scrolled");
+        } else {
+            $("header").removeClass("scrolled");
+        }
+    }
+    if($("header").length > 0) {
+        $(window).on("scroll load resize", function(){
+            checkScroll();
+        });
+    }
+
+    // If scroll postion is not at the top then call function
+    if(scrollPosition > 25){
+        checkScroll();
+    }
+
+    function showNav() {
+    	if ($(window).width() >= 1024) {
+    		$("nav").show();
+    	} else {
+    		$("nav").hide();
+    	}
+    }
+
+    window.onresize = showNav;
 });
