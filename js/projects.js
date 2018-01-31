7df77d2ea0a951f01c547495dbb25a10c37b8793
Fawaz Mohamed <fwz.mhmd@gmail.com>
2018-01-31 23:37:09 +0000

    Add projects in array

---
+++ js/projects.js
@@ -0,0 +1,38 @@
+const projects = [
+    {
+        name: 'RSVP Event Management',
+        description: 'RSVP app for event management',
+        img_url: '300.png',
+        source: 'rsvp'
+    },
+    {
+        name: 'Dice Game',
+        description: 'Simple dice game between 2 players',
+        img_url: '300.png',
+        source: 'dice-game'
+    },
+    {
+        name: 'Bookmarker',
+        description: 'Bookmark application to save favourite websites',
+        img_url: '300.png',
+        source: 'bookmarker'
+    },
+    {
+        name: 'Weight Convertor',
+        description: 'convert weight/mass between various measurements',
+        img_url: '300.png',
+        source: 'rsvp'
+    },
+    {
+        name: 'Tribute Page',
+        description: 'Tribute page for Web development course',
+        img_url: '300.png',
+        source: 'tribute-page'
+    },
+    {
+        name: 'BMI Calculator',
+        description: 'BMI calculator written in Javascript',
+        img_url: '300.png',
+        source: 'bmi'
+    }
+];
