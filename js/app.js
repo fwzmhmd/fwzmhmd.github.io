f06cadac0f80b4233181d076e9b442f716fa69e4
Fawaz Mohamed <fwz.mhmd@gmail.com>
2018-02-04 17:57:08 +0000

    Rewrite script

--- js/app.js
+++ js/app.js
@@ -1,54 +1,50 @@
-const projectsSection = document.querySelector('.projects .container');
 const projectsContainer = document.querySelector('.projectsContainer');
 
-
-projectsSection.appendChild(projectsContainer);
-
-for (let i = 0; i < projects.length; i++) {
-    const projectsList = projects[i];
-    const cardContent = document.createElement('div');
-    cardContent.className = 'cardContent clearfix';
-
-    const btnCont = document.createElement('div');
-    const btnCont2 = document.createElement('div');
-    btnCont.className = 'btnCont';
-    btnCont2.className = 'btnCont2';
-
-    const card = document.createElement('div');
-    card.className = 'card ' + 'card-' + (i + 1);
-    
-    const image = document.createElement('img');
-    image.src = 'img/' + projectsList.img_url;
-    card.appendChild(image);
-
-    const name = document.createElement('div');
-    name.className = 'cardTitle';
-    cardContent.appendChild(name);
-    name.textContent = projectsList.name;
-
-    const description = document.createElement('div');
-    description.className = 'cardDescription';
-    cardContent.appendChild(description);
-    description.textContent = projectsList.description;
-
-    const url = document.createElement('a');
-    url.href = 'https://fwzmhmd.github.io/' + projectsList.source;
-    url.className = 'btn btn-view';
-    url.textContent = 'view';
-    url.target = '_blank';
-    btnCont.appendChild(url);
-    cardContent.appendChild(btnCont);
-    
-    const source = document.createElement('a');
-    source.href = 'https://github.com/fwzmhmd/' + projectsList.source;
-    source.className = 'btn btn-source';
-    source.textContent = 'source code';
-    source.target = '_blank';
-    btnCont2.appendChild(source);
-    cardContent.appendChild(btnCont2);
-
-    card.appendChild(cardContent);
-    
-    projectsContainer.appendChild(card);
+function createElement(elementName, propertyName, value) {
+    const element = document.createElement(elementName);
+    element[propertyName] = value;
+    return element;
 }
 
+for (let i = 0; i < projects.length; i++) {
+    const cardContainer = createElement('div', 'className', 'cardContainer');
+    const card = createElement('div', 'className', 'card');
+
+    const thumbnail = createElement('img', 'className', 'thumbnail');
+    thumbnail.src = 'img/' + projects[i].img_url;
+    card.appendChild(thumbnail);
+
+    const cardContent = createElement('div', 'className', 'cardContent');
+    card.appendChild(cardContent);
+    
+    const title = createElement('div', 'className', 'title');
+    title.textContent = projects[i].name;
+    cardContent.appendChild(title);
+    
+    const description = createElement('div', 'className', 'description');
+    description.textContent = projects[i].description;
+    cardContent.appendChild(description);
+
+    const btnGroup = createElement('div', 'className', 'btnGroup clearfix');
+
+    const btnCont = createElement('div', 'className', 'btnCont');
+    const url = createElement('a', 'className', 'btn btn-view');
+    url.textContent = 'view';
+    url.target = '_blank';
+    url.href = 'https://fwzmhmd.github.io/' + projects[i].source;
+    btnCont.appendChild(url);
+    btnGroup.appendChild(btnCont);
+    card.appendChild(btnGroup);
+    
+    const btnCont2 = createElement('div', 'className', 'btnCont2');
+    const source = createElement('a', 'className', 'btn btn-source');
+    source.textContent = 'source';
+    source.target = '_blank';
+    source.href = 'https://github.com/fwzmhmd/' + projects[i].source;
+    btnCont2.appendChild(source);
+    btnGroup.appendChild(btnCont2);
+    card.appendChild(btnGroup);
+    
+    cardContainer.appendChild(card);
+    projectsContainer.appendChild(cardContainer);
+}
