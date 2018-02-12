11312b7050a0b58e9bb7d32e302afb396d5edd64
Fawaz Mohamed <fwz.mhmd@gmail.com>
2018-02-12 17:55:24 +0000

    Use seperate file for minified files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
-const projectsContainer=document.querySelector('.projectsContainer');function createElement(elementName,propertyName,value){const element=document.createElement(elementName);element[propertyName]=value;return element}for(let i=0;i<projects.length;i++){const cardContainer=createElement('div','className','cardContainer');const card=createElement('div','className','card');const thumbnail=createElement('img','className','thumbnail');thumbnail.src='img/'+projects[i].img_url;card.appendChild(thumbnail);const cardContent=createElement('div','className','cardContent');card.appendChild(cardContent);const title=createElement('div','className','title');title.textContent=projects[i].name;cardContent.appendChild(title);const description=createElement('div','className','description');description.textContent=projects[i].description;cardContent.appendChild(description);const btnGroup=createElement('div','className','btnGroup clearfix');const btnCont=createElement('div','className','btnCont');const url=createElement('a','className','btn btn-view');url.textContent='view';url.target='_blank';url.href='https://fwzmhmd.github.io/'+projects[i].source;btnCont.appendChild(url);btnGroup.appendChild(btnCont);card.appendChild(btnGroup);const btnCont2=createElement('div','className','btnCont2');const source=createElement('a','className','btn btn-source');source.textContent='source code';source.target='_blank';source.href='https://github.com/fwzmhmd/'+projects[i].source;btnCont2.appendChild(source);btnGroup.appendChild(btnCont2);card.appendChild(btnGroup);cardContainer.appendChild(card);projectsContainer.appendChild(cardContainer)}
+const projectsContainer = document.querySelector('.projectsContainer');
+
+function createElement(elementName, propertyName, value) {
+    const element = document.createElement(elementName);
+    element[propertyName] = value;
+    return element;
+}
+
+for (var i = 0; i < projects.length; i++) {
+    const cardContainer = createElement('div', 'className', 'cardContainer');
+    const card = createElement('div', 'className', 'card');
+
+    const thumbnail = createElement('img', 'className', 'thumbnail');
+    thumbnail.src = 'img/' + projects[i].img_url;
+    card.appendChild(thumbnail);
+    
+    const cardContent = createElement('div', 'className', 'cardContent');
+    card.appendChild(cardContent);
+
+    const title = createElement('div', 'className', 'title');
+    title.textContent = projects[i].name;
+    cardContent.appendChild(title);
+
+    const description = createElement('div', 'className', 'description');
+    description.textContent = projects[i].description;
+    cardContent.appendChild(description);
+
+    const btnGroup = createElement('div', 'className', 'btnGroup clearfix');
+    const btnCont = createElement('div', 'className', 'btnCont');
+    const url = createElement('a', 'className', 'btn btn-view');
+    url.textContent = 'view';
+    url.target = '_blank';
+    url.href = 'https://fwzmhmd.github.io/' + projects[i].source;
+    btnCont.appendChild(url);
+    btnGroup.appendChild(btnCont);
+    card.appendChild(btnGroup);
+
+    const btnCont2 = createElement('div', 'className', 'btnCont2');
+    const source = createElement('a', 'className', 'btn btn-source');
+    source.textContent = 'source code';
+    source.target = '_blank';
+    source.href = 'https://github.com/fwzmhmd/' + projects[i].source;
+    btnCont2.appendChild(source);
+    btnGroup.appendChild(btnCont2);
+    card.appendChild(btnGroup);
+    cardContainer.appendChild(card);
+    projectsContainer.appendChild(cardContainer);
+}
